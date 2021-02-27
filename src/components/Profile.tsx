c6328310f28c2af188919c74d9e7f61088d2d1f8
Tárik Wataya <tarikwataya@gmail.com>
2021-02-26 23:13:18 -0300

    Adicionando funcionalidade de levelup e novos contexts
    -Novo context Countdown
    -Funcionalidade de subir de nivel
    -Ajuste nos botoes de complete e falha do desafio

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,9 @@
-import { Profiler } from 'react';
+import { Profiler, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const {level} = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/tarikwataya.png" alt="" />
@@ -9,7 +11,7 @@ export function Profile() {
                 <strong>Tárik Wataya</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -48,8 +49,37 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         setActiveChallenge(null);
     }
 
+    function completeChallenge() {
+        if (!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
     return (
-        <ChallengesContext.Provider value={{ level, currentExperience, experienceToNextLevel, challengesCompleted, levelUp, startNewChallenge, activeChallenge, resetChallenge, }}>
+        <ChallengesContext.Provider value={{
+            level, 
+            currentExperience, 
+            experienceToNextLevel, 
+            challengesCompleted, 
+            levelUp, 
+            startNewChallenge, 
+            activeChallenge, 
+            resetChallenge, 
+            completeChallenge,
+            }}>
             {children}
         </ChallengesContext.Provider>
     );
